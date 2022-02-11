559a0dca555241fef1edcfab5942156ccab2ca48
Lara <git@lara.uber.space>
2022-02-11 07:59:24 +0100

    Set usb drive name

--- core.py
+++ core.py
@@ -3,30 +3,35 @@ import board
 import digitalio
 from neopixel import NeoPixel
 import usb_hid
+import storage
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-muted = False
-buttonStillPressed = False
-
+# name usb drive
+storage.remount("/", readonly=False)
+m = storage.getmount("/")
+m.label = "MUTEKEY"
+storage.remount("/", readonly=True)
+storage.enable_usb_drive()
 # storage.disable_usb_device()
-# usb_midi.disable()
 
 pixel = NeoPixel(board.NEOPIXEL, 1)
 pixel.brightness = 0.05
 pixel[0] = 0x00FF00
 
-kbd = Keyboard(usb_hid.devices)
-
 button = digitalio.DigitalInOut(board.SWITCH)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
+kbd = Keyboard(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
 
 print("neokey up")
+
+muted = False
+buttonStillPressed = False
 
 while True:
     if button.value and not buttonStillPressed:
