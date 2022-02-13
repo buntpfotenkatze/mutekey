12c8951cc0672cb11401c952f75d2e3f5cfae24e
Lara <git@lara.uber.space>
2022-02-13 21:58:21 +0100

    Change keycode for unmute

--- core.py
+++ core.py
@@ -4,17 +4,15 @@ import digitalio
 from neopixel import NeoPixel
 import usb_hid
 import storage
-from adafruit_hid.consumer_control import ConsumerControl
-from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
 # name usb drive
-storage.remount("/", readonly=False)
-m = storage.getmount("/")
-m.label = "MUTEKEY"
-storage.remount("/", readonly=True)
-storage.enable_usb_drive()
+#storage.remount("/", readonly=False)
+#m = storage.getmount("/")
+#m.label = "MUTEKEY"
+#storage.remount("/", readonly=True)
+#storage.enable_usb_drive()
 # storage.disable_usb_device()
 
 pixel = NeoPixel(board.NEOPIXEL, 1)
@@ -26,18 +24,19 @@ button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
 kbd = Keyboard(usb_hid.devices)
-cc = ConsumerControl(usb_hid.devices)
-
-print("neokey up")
 
 muted = False
 buttonStillPressed = False
+
+print("neokey up")
 
 while True:
     if button.value and not buttonStillPressed:
         buttonStillPressed = True
         if muted:
-            cc.send(ConsumerControlCode.RECORD)
+            kbd.send(
+                Keycode.LEFT_SHIFT, Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.PAUSE
+            )
             pixel[0] = 0x00FF00
             muted = False
         else:
